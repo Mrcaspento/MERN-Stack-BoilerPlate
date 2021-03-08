fe9b503d10eb8e5287f7eee22bae712132a98056
Caspento <Mrcaspento@gmail.com>
2021-03-08 16:14:54 -0500

    init push

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM, {hydrate, render} from "react-dom";
+import Routes from "./Routes";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<Routes />, rootElement);
+} else {
+  render(<Routes />, rootElement);
+}
